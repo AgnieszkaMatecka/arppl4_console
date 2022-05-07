23cdceadc1105df3deef73c54570f9b5c1d5bb2e
A <maawi1253@gmail.com>
2022-05-07 10:29:16 +0200

    Commit scanner

--- src/main/java/pl/sda/arp4/console/Main.java
+++ src/main/java/pl/sda/arp4/console/Main.java
@@ -1,4 +1,13 @@
 package pl.sda.arp4.console;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj swoje imie:");
+        String zmiennaImie = scanner.next();
+        System.out.println("Witaj " + zmiennaImie + "!");
+    }
 }
---
+++ src/main/java/pl/sda/arp4/console/MainDekady.java
@@ -0,0 +1,14 @@
+package pl.sda.arp4.console;
+
+import java.util.Scanner;
+
+public class MainDekady public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Podaj swoj wiek:");
+
+    int zmiennaWiek = scanner.nextInt();
+    int wiekDekady = zmiennaWiek/10;
+
+    System.out.println("Żyjesz już " + wiekDekady + " dekad!");
+}
